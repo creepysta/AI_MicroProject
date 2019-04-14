4ec92e5aa7acc2b2970bae6e00b926eb39aa92b3
creepysta <sweetnanispregnantghost@yahoo.com>
2019-04-14 09:10:02 +0530

    generalised for every square matrix

--- TC_HebbClassification.py
+++ TC_HebbClassification.py
@@ -7,28 +7,28 @@ def tc_classifier(t, c):
     yc = -1
     # training
     for test in range(2):
-        for i in range(3):
-            for j in range(3):
+        for i in range(len(xt)):
+            for j in range(len(xt[0])):
                 if test == 0:
                     weights[i][j] = weights[i][j] + yt * xt[i][j]
                 else:
                     weights[i][j] = weights[i][j] + yc * xc[i][j]
         bias = bias + 1
-        weights[2].append(bias)
-        print(weights)
+
+    weights.append(bias)
 
     # testing
     for test in range(2):
         verify = 0
-        for i in range(3):
-            for j in range(3):
+        for i in range(len(xt)):
+            for j in range(len(xt[0])):
                 if test == 0:
                     verify = verify + weights[i][j] * t[i][j]
                 else:
                     verify = verify + weights[i][j] * c[i][j]
-        if test == 0 and verify + weights[2][3] > 0:
+        if verify + weights[i+1] > 0:
             print("Entered matrix is T")
-        elif test == 1 and verify + weights[2][3] < 0:
+        elif verify + weights[i+1] < 0:
             print("Entered matrix is C")
         else:
             print('Not recognised')
@@ -37,4 +37,8 @@ def tc_classifier(t, c):
 t = [[1, 1, 1], [-1, 1, -1], [-1, 1, -1]]
 c = [[1, 1, 1], [1, -1, -1], [1, 1, 1]]
 
+print("Matrix 1: ",  t)
+print("Matrix 2: ",  c)
+
 tc_classifier(t, c)
+
